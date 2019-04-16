91c0fc5b532295d9997c98eb1241e52ea4cd75dc
Nedson202 <simeonsamson4@gmail..com>
2019-04-16 13:24:00 +0100

    Update randomizer function

--- helper/dataRandomiser.js
+++ helper/dataRandomiser.js
@@ -1,6 +1,7 @@
 const randomizer = (min, max) => {
-  const digits = (max - min) + min;
-  return Math.floor(Math.random() * digits);
+  const digits = (max - min);
+  const random = Math.floor(Math.random() * digits) + min;
+  return random;
 };
 
 export default randomizer;
