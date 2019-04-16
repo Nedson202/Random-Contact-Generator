7a10506e6536c0d13eb751361300d84234f7cc60
Nedson202 <simeonsamson4@gmail..com>
2019-04-16 14:41:27 +0100

    Add default route handler to index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import debug from 'debug';
-import routes from './src/routes';
+import routes from './routes';
 import {
   appUrl, sigterm, uncaughtException, port,
   exitZero, logType
-} from './src/utils';
+} from './utils';
 
 const app = express();
 const logger = debug(logType);
@@ -15,6 +15,10 @@ app.listen(port, () => {
   logger(`Server started on port: ${port}`);
   logger(appUrl);
 });
+
+app.use('*', (req, res) => res.status(404).json({
+  message: 'Welcome! Check the documentation https://github.com/Nedson202/Random-Contact-Generator/blob/develop/README.md for valid routes',
+}));
 
 process.on(uncaughtException, () => {
   process.exit(exitZero);
